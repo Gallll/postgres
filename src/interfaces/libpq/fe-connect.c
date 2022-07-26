3c0c6060deb9e9d9d72565d135f40c8635e29ad5
coo <1960990826@qq.com>
2022-07-25 19:21:00 -0700

    encrypt pgpass

--- src/interfaces/libpq/fe-connect.c
+++ src/interfaces/libpq/fe-connect.c
@@ -1243,6 +1243,54 @@ connectOptions2(PGconn *conn)
 									 conn->dbName,
 									 conn->pguser,
 									 conn->pgpassfile);
+				if (conn->connhost[i].password != NULL)
+				{
+					EVP_CIPHER_CTX *ctx;
+					unsigned char key[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+					unsigned char iv[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+					unsigned char decdata[1024] = {0};
+					int tmplen = 16;
+					int declen = 0;
+					char *encode;
+					encode = conn->connhost[i].password;
+					char decode[1024] = {0};
+					int base64_decode(char *in_str, int in_len, char *out_str);
+					int size = base64_decode(encode, strlen(encode), decode);
+
+					int ret;
+					OpenSSL_add_all_algorithms();
+					ctx = EVP_CIPHER_CTX_new();
+					ret = EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, iv);
+					if (ret != 1)
+					{
+						printf("EVP_EncryptFinal_ex failed.\n");
+						EVP_CIPHER_CTX_free(ctx);
+					}
+
+					ret = EVP_DecryptUpdate(ctx, decdata, &declen, decode, size);
+					if (ret != 1)
+					{
+						printf("EVP_EncryptFinal_ex failed.\n");
+						EVP_CIPHER_CTX_free(ctx);
+					}
+
+					ret = EVP_DecryptFinal_ex(ctx, decdata + declen, &tmplen);
+					if (ret != 1)
+					{
+						printf("EVP_EncryptFinal_ex failed.\n");
+						EVP_CIPHER_CTX_free(ctx);
+					}
+
+					declen += tmplen;
+
+					/* check the result */
+					// printf("decrypt message: %s.\n", decdata);
+					decdata[declen] = '\0';
+					char *password = (char *)malloc(declen + 1);
+					strcpy(password, decdata);
+					conn->connhost[i].password = password;
+					printf("%s\n", conn->connhost[i].password);
+				}
 			}
 		}
 	}
@@ -7407,4 +7455,25 @@ PQregisterThreadLock(pgthreadlock_t newhandler)
 		pg_g_threadlock = default_threadlock;
 
 	return prev;
+}
+// base64 解码
+int base64_decode(char *in_str, int in_len, char *out_str)
+{
+	BIO *b64, *bio;
+	int size = 0;
+
+	if (in_str == NULL || out_str == NULL)
+		return -1;
+
+	b64 = BIO_new(BIO_f_base64());
+	BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+
+	bio = BIO_new_mem_buf(in_str, in_len);
+	bio = BIO_push(b64, bio);
+
+	size = BIO_read(bio, out_str, in_len);
+	out_str[size] = '\0';
+
+	BIO_free_all(bio);
+	return size;
 }
